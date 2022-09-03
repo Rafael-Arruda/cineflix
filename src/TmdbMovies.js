5347a96d50b9210f7425ce4faf4b13f241f7431e
Rafael-Arruda <rafaelsilvaarruda72@gmail.com>
2022-09-03 00:41:07 -0300

    Página de Filmes

---
+++ src/TmdbMovies.js
@@ -0,0 +1,57 @@
+import api from './services/api';
+
+// const API_KEY = 'dc8d1f407a1bd3c7756a115230fc20e7';
+// const API_BASE = 'https://api.themoviedb.org/3';
+
+const basicFetch = async (endpoint) => {
+    const response = await api.get(`${endpoint}`, {
+        params: {
+            api_key: 'dc8d1f407a1bd3c7756a115230fc20e7',
+            language: 'pt-BR',
+            page: 1
+        }
+    });
+    return response.data.results;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'nowplaying',
+                title: 'Lançados recentemente',
+                items: await basicFetch(`/movie/now_playing`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: await basicFetch(`/movie/top_rated`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35`)
+            },
+            {
+                slug: 'fantasy',
+                title: 'Fantasia',
+                items: await basicFetch(`/discover/movie?with_genres=14`)
+            },
+            {
+                slug: 'crime',
+                title: 'Crime',
+                items: await basicFetch(`/discover/movie?with_genres=80`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27`)
+            },
+        ];
+    }
+}
--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import {Container} from './style';
+
+import Featured from "../../components/Featured";
+import ListRow from '../../components/ListRow';
+import Footer from '../../components/Footer';
+import TmdbMovies from "../../TmdbMovies";
 
 function Movies() {
+
+    const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
+
+    useEffect(() => {
+        const loadAll = async () => {
+            //Pegando a lista total
+            let list = await TmdbMovies.getHomeList();
+            setMovieList(list);
+
+            //Pegando o Featured
+            let nowPlaying = list.filter(list => list.slug === 'nowplaying')
+            let randomChosenIndex = Math.floor(Math.random() * (nowPlaying[0].items.length));
+            let movieChosen = nowPlaying[0].items[randomChosenIndex];
+            
+            setFeaturedData(movieChosen);
+        }
+
+        loadAll();
+    }, []);
+
     return(
-        <div>
-            <h2>Movies</h2>
-        </div>
+        <Container>
+
+            {featuredData && 
+                <Featured item={featuredData}/>
+            }
+            
+            {movieList.map((list) => (
+                <ListRow key={list.slug} title={list.title} list={list.items}/>
+            ))}
+
+            <Footer/>
+
+        </Container>
     )
 }
 
