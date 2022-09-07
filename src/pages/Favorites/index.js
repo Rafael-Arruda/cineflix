419ffcaa933be06884ad11e66d07fcfaa26f2af3
Rafael-Arruda <rafaelsilvaarruda72@gmail.com>
2022-09-06 21:25:43 -0300

    Deletar da lista de favoritos

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import {Container} from './style';
 
+import {MdClose} from 'react-icons/md'
+
 import {Link} from 'react-router-dom';
 
 function Favorites(){
@@ -18,6 +20,13 @@ function Favorites(){
         getFavorites();
     }, [])
 
+    function deleteVideo(id){
+        let currentList = favorites.filter(item => item.video.id !== id);
+
+        localStorage.setItem('@cineflix', JSON.stringify(currentList));
+        setFavorites(currentList);
+    }
+
     return(
         <Container>
             <div className="list">
@@ -26,16 +35,21 @@ function Favorites(){
                     const releaseDate = new Date(item.type === 'movie'? item.video.release_date : item.video.first_air_date);
                     
                     return(
-                        <Link to={`/details/${item.type}/${item.video.id}`} className="box-card" key={item.video.id}>
-                            <img src={`https://image.tmdb.org/t/p/w200/${item.video.backdrop_path}`} alt={item.title}/>
-                            <div className="info-card">
-                                <h4>{item.type === 'movie'? item.video.title : item.video.name}</h4>
-                                <p>{releaseDate.getFullYear()}</p>
-                            </div>
-                        </Link>
+                        <div key={item.video.id} className="box-card">
+                            <Link title='Ver detalhes' to={`/details/${item.type}/${item.video.id}`}>
+                                <img src={`https://image.tmdb.org/t/p/w200/${item.video.backdrop_path}`} alt={item.title}/>
+                                <div className="info-card">
+                                    <h4>{item.type === 'movie'? item.video.title : item.video.name}</h4>
+                                    <p>{releaseDate.getFullYear()}</p>
+                                </div>
+                            </Link>
+
+                            <MdClose onClick={() => deleteVideo(item.video.id)} title="Remover da lista" size={20}/>
+                        </div>
                     )
                 })}
             </div>  
+
         </Container>
     )
 }
--- src/pages/Favorites/style.js
+++ src/pages/Favorites/style.js
@@ -3,25 +3,41 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100%;
 
-    .list {
-        
-    }
-
     .box-card {
         padding: 10px 15px;
         display: flex;
+        justify-content: space-between;
         /* background-color: #0f171e; */
 
         :not(:first-child) {
             border-top: 2px solid #ddd;
         }
+
+        a {
+            display: flex;
+        }
+
+        svg {
+            cursor: pointer;
+            border-radius: 2px;
+            color: #fff;
+            background-color: #0f79af;
+        }
+        svg:hover {
+            color: #ddd;
+        }
     }
 
     .info-card {
         padding: 0px 5px;
+        color: #fff;
 
         h4 {
             font-weight: 500;
+            transition: 0.2s all;
+        }
+        h4:hover {
+            color: #0f79af;
         }
 
         p {
